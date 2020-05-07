f798a0debbc77b6436c1ff7fd07c7d85602dfc5c
Dilanka Dharmasena <git@dilankadharmasena.com>
2020-05-07 16:31:47 -0400

    Initial commit

---
+++ src/test/java/com/sustral/clientapi/ClientapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sustral.clientapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClientapiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
